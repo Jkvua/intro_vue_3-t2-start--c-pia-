4e949ffb8e7d66d107b0eca7b033e2b73f84e269
camila farreira de almeida <mila.e.ferreira28@gmail.com>
2024-04-19 09:40:48 -0300

    atividade de viu simples

---
+++ intro_vue_3-t2-start/main.js
@@ -0,0 +1,13 @@
+//const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            description: 'Meias para aquecer os pés'
+        }
+    }
+
+})
+
+//const app = Vue.createApp({Options Object})
