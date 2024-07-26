8a7e8d8dadae9ae47c60134173dcb31604af191a
camila farreira de almeida <mila.e.ferreira28@gmail.com>
2024-07-26 09:15:56 -0300

    tutorial part 4 - como funciona o vue-router

--- PJ-vue/vue_3_mundo_real/real-world-vue/src/App.vue
+++ PJ-vue/vue_3_mundo_real/real-world-vue/src/App.vue
@@ -7,12 +7,12 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Events</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
     </header>
-    <RouterView />
+    <RouterView /> <!--componente que vincula a rota especifica-->
   </div>
 </template>
 
--- PJ-vue/vue_3_mundo_real/real-world-vue/src/main.js
+++ PJ-vue/vue_3_mundo_real/real-world-vue/src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './router' /* estamos importando o index.js do diretorio router */
 
 const app = createApp(App)
 
